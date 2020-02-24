12d9846d7ae8619e5607bdb56a620055c880c2d5
Viktor Abrahamsson <a17vikab@student.his.se>
2020-02-24 23:24:18 +0100

    Remove old comments
    In app.js

--- pilot_heatmap/scripts/app.js
+++ pilot_heatmap/scripts/app.js
@@ -1,101 +1,3 @@
-// class JsonData {
-//   async getJsonData() {
-//     try {
-//       let result = await fetch("dataset.json");
-//       let data = await result.json();
-//       data = data.map(object => {
-//         const lat = data.latitude;
-//         const long = data.longitude;
-//         const brightness = data.brightness;
-//         return { lat, long, brightness };
-//       });
-//       return data;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// }
-
-// async function getJsonData() {
-//   let result = await fetch("dataset.json");
-//   let data = await result.json();
-//   data = data.map(object => {
-//     const lat = data.latitude;
-//     const long = data.longitude;
-//     const brightness = data.brightness;
-//     return { lat, long, brightness };
-//   });
-//   return data;
-// }
-
-// const getJsonData = () => {};
-
-// var testData = {
-//   max: 8,
-//   data: [
-//     {
-//       latitude: -4.6657,
-//       longitude: 143.865,
-//       brightness: 305.8
-//     },
-//     {
-//       latitude: -15.8191,
-//       longitude: 135.4216,
-//       brightness: 328.3
-//     }
-//   ]
-// };
-
-// const getJsonData = () => {
-//   let result = await fetch("dataset.json");
-//   let data = await result.json();
-//   data = data.map(object => {
-//     const lat = data.latitude;
-//     const long = data.longitude;
-//     const brightness = data.brightness;
-//     return { lat, long, brightness };
-//   })
-//   return data;
-// };
-
-// latField: "latitude",
-// lngField: "longitude",
-// valueField: "brightness"
-
-// var testData = {
-//   max: 8,
-//   data: Json.getJsonData()
-// };
-
-// const { latitude, longitude, brightness } = require("../dataset.json");
-// console.log(latitude);
-
-// var jsonData = {
-//   max: 8,
-//   data: fetch("./dataset.json")
-//     .then(function(resp) {
-//       return resp.json();
-//     })
-//     .then(function(data) {
-//       JSON.parse(data);
-//     })
-// };
-
-// fetch("./dataset.json")
-//   .then(function(resp) {
-//     return resp.json();
-//   })
-//   .then(function(data) {
-//     const fetchedData = data;
-//     return fetchedData;
-//   });
-
-// $(document).ready(function() {
-//   var json = require("../dataset.json");
-// });
-
-// import * as importData from "../dataset.json";
-
 function loadJSON(callback) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
@@ -127,7 +29,7 @@ function init() {
     );
 
     var cfg = {
-      radius: 1,
+      radius: 0.5,
       maxOpacity: 0.8,
       scaleRadius: true,
       useLocalExtrema: true,
