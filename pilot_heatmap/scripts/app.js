77ebf46772298d7457756f7dabada85a5b25acf5
Viktor Abrahamsson <a17vikab@student.his.se>
2020-02-24 23:22:55 +0100

    IT IS ALIVE
    I finaly got heatmap.js / Leaflet.js and the dataset to work. I will now go to bed and sleep.

--- pilot_heatmap/scripts/app.js
+++ pilot_heatmap/scripts/app.js
@@ -1,106 +1,151 @@
-class Json {
-  async getJsonData() {
-    try {
-      let result = await fetch("dataset.json");
-      let data = await result.json();
-      data = data.map(object => {
-        const lat = data.latitude;
-        const long = data.longitude;
-        const brightness = data.brightness;
-        return { lat, long, brightness };
-      });
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
+// class JsonData {
+//   async getJsonData() {
+//     try {
+//       let result = await fetch("dataset.json");
+//       let data = await result.json();
+//       data = data.map(object => {
+//         const lat = data.latitude;
+//         const long = data.longitude;
+//         const brightness = data.brightness;
+//         return { lat, long, brightness };
+//       });
+//       return data;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function getJsonData() {
+//   let result = await fetch("dataset.json");
+//   let data = await result.json();
+//   data = data.map(object => {
+//     const lat = data.latitude;
+//     const long = data.longitude;
+//     const brightness = data.brightness;
+//     return { lat, long, brightness };
+//   });
+//   return data;
+// }
+
+// const getJsonData = () => {};
+
+// var testData = {
+//   max: 8,
+//   data: [
+//     {
+//       latitude: -4.6657,
+//       longitude: 143.865,
+//       brightness: 305.8
+//     },
+//     {
+//       latitude: -15.8191,
+//       longitude: 135.4216,
+//       brightness: 328.3
+//     }
+//   ]
+// };
+
+// const getJsonData = () => {
+//   let result = await fetch("dataset.json");
+//   let data = await result.json();
+//   data = data.map(object => {
+//     const lat = data.latitude;
+//     const long = data.longitude;
+//     const brightness = data.brightness;
+//     return { lat, long, brightness };
+//   })
+//   return data;
+// };
 
 // latField: "latitude",
 // lngField: "longitude",
 // valueField: "brightness"
 
-var testData = {
-  max: 8,
-  data: [
-    {
-      latitude: -4.6657,
-      longitude: 143.865,
-      acq_date: "2019-03-01",
-      acq_time: "0101",
-      brightness: 305.8,
-      daynight: "D",
-      bright_t31: 286.5,
-      type: "0",
-      version: "6.3",
-      satellite: "Terra",
-      confidence: "18",
-      scan: 1.1,
-      track: 1.1,
-      frp: 4.7,
-      instrument: "MODIS"
-    },
-    {
-      latitude: -15.8191,
-      longitude: 135.4216,
-      acq_date: "2019-03-01",
-      acq_time: "0104",
-      brightness: 328.3,
-      daynight: "D",
-      bright_t31: 305.6,
-      type: "0",
-      version: "6.3",
-      satellite: "Terra",
-      confidence: "61",
-      scan: 1.4,
-      track: 1.2,
-      frp: 13.4,
-      instrument: "MODIS"
-    },
-    {
-      latitude: -15.8294,
-      longitude: 135.4197,
-      acq_date: "2019-03-01",
-      acq_time: "0104",
-      brightness: 330.3,
-      daynight: "D",
-      bright_t31: 306.5,
-      type: "0",
-      version: "6.3",
-      satellite: "Terra",
-      confidence: "73",
-      scan: 1.4,
-      track: 1.2,
-      frp: 16.7,
-      instrument: "MODIS"
+// var testData = {
+//   max: 8,
+//   data: Json.getJsonData()
+// };
+
+// const { latitude, longitude, brightness } = require("../dataset.json");
+// console.log(latitude);
+
+// var jsonData = {
+//   max: 8,
+//   data: fetch("./dataset.json")
+//     .then(function(resp) {
+//       return resp.json();
+//     })
+//     .then(function(data) {
+//       JSON.parse(data);
+//     })
+// };
+
+// fetch("./dataset.json")
+//   .then(function(resp) {
+//     return resp.json();
+//   })
+//   .then(function(data) {
+//     const fetchedData = data;
+//     return fetchedData;
+//   });
+
+// $(document).ready(function() {
+//   var json = require("../dataset.json");
+// });
+
+// import * as importData from "../dataset.json";
+
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "./dataset.json", true);
+  xobj.onreadystatechange = function() {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
     }
-  ]
-};
+  };
+  xobj.send(null);
+}
 
-var baseLayer = L.tileLayer(
-  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    attribution: "test",
-    maxZoom: 18
-  }
-);
+function init() {
+  loadJSON(function(response) {
+    var actual_JSON = JSON.parse(response);
+    const jsonResponse = response;
+    console.log(jsonResponse);
 
-var cfg = {
-  radius: 0.2,
-  maxOpacity: 0.8,
-  scaleRadius: true,
-  useLocalExtrema: true,
-  latField: "latitude",
-  lngField: "longitude",
-  valueField: "brightness"
-};
+    var jsonData = {
+      data: actual_JSON
+    };
 
-var heatmapLayer = new HeatmapOverlay(cfg);
+    var baseLayer = L.tileLayer(
+      "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        attribution: "HeatmapTest",
+        maxZoom: 18
+      }
+    );
 
-var map = new L.Map("map", {
-  center: new L.LatLng(0, 0),
-  zoom: 2,
-  layers: [baseLayer, heatmapLayer]
-});
+    var cfg = {
+      radius: 1,
+      maxOpacity: 0.8,
+      scaleRadius: true,
+      useLocalExtrema: true,
+      latField: "latitude",
+      lngField: "longitude",
+      valueField: "brightness"
+    };
 
-heatmapLayer.setData(testData);
+    var heatmapLayer = new HeatmapOverlay(cfg);
+
+    var map = new L.Map("map", {
+      center: new L.LatLng(0, 0),
+      zoom: 2,
+      layers: [baseLayer, heatmapLayer]
+    });
+
+    heatmapLayer.setData(jsonData);
+  });
+}
+
+init();
