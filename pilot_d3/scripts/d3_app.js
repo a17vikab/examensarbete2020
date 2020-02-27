20e15d1b8ced2be8e07c5503ee32dd3deb2f40fd
Viktor Abrahamsson <a17vikab@student.his.se>
2020-02-27 14:23:45 +0100

    Working Leaflet with on d3-file

--- pilot_d3/scripts/d3_app.js
+++ pilot_d3/scripts/d3_app.js
@@ -1,53 +1,15 @@
-function loadJSON(callback) {
-  var obj = new XMLHttpRequest();
-  obj.overrideMimeType("application/json");
-  obj.open("GET", "./dataset.json", true);
-  obj.onreadystatechange = function() {
-    if (obj.readyState == 4 && obj.status == "200") {
-      callback(obj.responseText);
-    }
-  };
-  obj.send(null);
-}
-
 function init() {
-  loadJSON(function(response) {
-    let jsonDataResponse = JSON.parse(response);
-    //console.log(jsonDataResponse);
-    let jsonData = {
-      data: jsonDataResponse
-    };
-  });
+  let map = L.map("map").setView([20, 20], 3);
 
-  let leafletLayer = L.tileLayer(
+  L.tileLayer(
     "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
     {
-      attribution: "D3Pilot",
-      maxZoom: 18
+      attribution: "HeatmapPilot",
+      maxZoom: 12
     }
-  );
+  ).addTo(map);
 
-  let map = new L.Map("map", {
-    center: new L.LatLng(20, 20),
-    zoom: 3,
-    layers: [leafletLayer]
-  });
-
-  let svg = d3.select("#map").select("svg"),
-    g = svg.append("g");
-
-  d3.json(
-    loadJSON(function(response) {
-      let jsonDataResponse = JSON.parse(response);
-      //console.log(jsonDataResponse);
-      let jsonData = {
-        data: jsonDataResponse
-      };
-    }),
-    function(error, data) {}
-  );
-
-  let d3Layer;
+  L.svg().addTo(map);
 }
 
 init();
