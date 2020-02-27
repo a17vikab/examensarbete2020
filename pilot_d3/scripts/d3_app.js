a1a85aa9b146607b0d39635e3d44f5ad8d44fe30
Viktor Abrahamsson <a17vikab@student.his.se>
2020-02-27 14:28:33 +0100

    Add function loadJSON

--- pilot_d3/scripts/d3_app.js
+++ pilot_d3/scripts/d3_app.js
@@ -1,15 +1,35 @@
 function init() {
-  let map = L.map("map").setView([20, 20], 3);
+  loadJSON(function(response) {
+    let jsonResponse = JSON.parse(response);
 
-  L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
-    {
-      attribution: "HeatmapPilot",
-      maxZoom: 12
+    let jsonData = {
+      data: jsonResponse
+    };
+
+    let map = L.map("map").setView([20, 20], 3);
+
+    L.tileLayer(
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
+      {
+        attribution: "HeatmapPilot",
+        maxZoom: 12
+      }
+    ).addTo(map);
+
+    L.svg().addTo(map);
+  });
+}
+
+function loadJSON(callback) {
+  let obj = new XMLHttpRequest();
+  obj.overrideMimeType("application/json");
+  obj.open("GET", "./dataset.json", true);
+  obj.onreadystatechange = function() {
+    if (obj.readyState == 4 && obj.status == "200") {
+      callback(obj.responseText);
     }
-  ).addTo(map);
-
-  L.svg().addTo(map);
+  };
+  obj.send(null);
 }
 
 init();
