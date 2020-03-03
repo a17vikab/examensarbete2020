e4d90b9005fee58da1b6568443cd5426c88f6852
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-03 11:00:40 +0100

    Quicksave before risky change

--- vued3/src/components/Heatmap.vue
+++ vued3/src/components/Heatmap.vue
@@ -1,45 +1,49 @@
 <template>
-  <div id="map"></div>
+  <div id="map">test</div>
 </template>
 
 <script>
+
+
 export default {
-  name: "Heatmap",
-  mounted() {
-    const leafletStyle = document.createElement("link");
-    leafletStyle
-      .setAttribute("rel", "stylesheet")
-      .setAttribute("href", "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css")
-      .setAttribute(
-        "integrity",
-        "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-      )
-      .setAttribute("crossorigin", "");
+  name: "Heatmap"
+  // mounted() {
+  //   const leafletStyle = document.createElement("link");
+  //   leafletStyle
+  //     .setAttribute("rel", "stylesheet")
+  //     .setAttribute("href", "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css")
+  //     .setAttribute(
+  //       "integrity",
+  //       "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  //     )
+  //     .setAttribute("crossorigin", "");
+  //   leafletStyle.async = true;
+  //   document.head.appendChild(leafletStyle);
 
-    const leafletScript = document.createElement("script");
-    leafletScript
-      .setAttribute("src", "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js")
-      .setAttribute(
-        "integrity",
-        "sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-      )
-      .setAttribute("crossorigin", "");
-    leafletScript.async = true;
-    document.head.appendChild(leafletScript);
+  //   const leafletScript = document.createElement("script");
+  //   leafletScript
+  //     .setAttribute("src", "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js")
+  //     .setAttribute(
+  //       "integrity",
+  //       "sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  //     )
+  //     .setAttribute("crossorigin", "");
+  //   leafletScript.async = true;
+  //   document.head.appendChild(leafletScript);
 
-    const d3Script1 = document.createElement("script");
-    d3Script1.setAttribute("src", "https://d3js.org/d3.v5.min.js");
-    d3Script1.async = true;
-    document.head.appendChild(d3Script1);
+  //   const d3Script1 = document.createElement("script");
+  //   d3Script1.setAttribute("src", "https://d3js.org/d3.v5.min.js");
+  //   d3Script1.async = true;
+  //   document.head.appendChild(d3Script1);
 
-    const d3Script2 = document.createElement("srcipt");
-    d3Script2.setAttribute(
-      "src",
-      "https://d3js.org/d3-geo-projection.v2.min.js"
-    );
-    d3Script2.async = true;
-    document.head.appendChild(d3Script2);
-  }
+  //   const d3Script2 = document.createElement("script");
+  //   d3Script2.setAttribute(
+  //     "src",
+  //     "https://d3js.org/d3-geo-projection.v2.min.js"
+  //   );
+  //   d3Script2.async = true;
+  //   document.head.appendChild(d3Script2);
+  // }
 };
 </script>
 
