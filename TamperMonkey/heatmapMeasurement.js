8ca8848f2062ebfaa6a192259efa72c776fffc91
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-05 14:50:00 +0100

    Create MeasurementScript

---
+++ TamperMonkey/heatmapMeasurement.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        heatmapMeasurement.js
+// @namespace   http://tampermonkey.net/
+// @version     0.1
+// @description Measurement-script for heatmap-data.
+// @author      Viktor Abrahamsson
+// @match       https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
+// @require     https://code.jquery.com/jquery-2.2.4.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+  "use strict";
+
+})();
