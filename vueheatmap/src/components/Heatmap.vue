d12f1eb48dff1a4733b43d0dcf44116a200ec040
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-05 13:21:59 +0100

    Quicksave

--- vueheatmap/src/components/Heatmap.vue
+++ vueheatmap/src/components/Heatmap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import L from "leaflet";
+import heatmap from "vue-heatmapjs";
 
 export default {
   name: "Heatmap",
@@ -13,7 +14,71 @@ export default {
     this.init();
   },
   methods: {
-    init() {}
+    init() {
+      function loadJSON(callback) {
+        // Create new instance of XMLHttpRequest
+        let obj = new XMLHttpRequest();
+        // Specifies type of data.
+        obj.overrideMimeType("application/json");
+        // Get the JSON-file.
+        obj.open("GET", "../Dataset/dataset.json", true);
+        // EventHandler for readystatechange.
+        obj.onreadystatechange = function() {
+          // Defining the error.
+          if (obj.readyState == 4 && obj.status == "200") {
+            // Throw an "InvalidStateError".
+            callback(obj.responseText);
+          }
+        };
+        // Initiates request.
+        obj.send(null);
+
+        loadJSON(function(response) {
+          // Parse the response in JSON-format.
+          let jsonResponse = JSON.parse(response);
+
+          // Store the parsed JSON-data.
+          let jsonData = {
+            data: jsonResponse
+          };
+
+          // Create LeafletLayer that holds the actual map.
+          let leafletLayer = L.tileLayer(
+            "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
+            {
+              attribution: "Vue.js + Heatmap.js",
+              maxZoom: 12
+            }
+          );
+
+          // Variable for config-values
+          let config = {
+            radius: 0.25,
+            maxOpacity: 0.8,
+            blur: 0.3,
+            scaleRadius: true,
+            useLocalExtrema: true,
+            // Linking JSON-file-values to Heatmap.js variables.
+            latField: "latitude",
+            lngField: "longitude",
+            valueField: "brightness"
+          };
+
+          // Create Layer and place config-variable in that overlay.
+          let heatmapLayer = new HeatmapOverlay(config);
+
+          // Create Map with Leaflet.js and set view, zoom and layers.
+          let map = new L.Map("map", {
+            center: new L.LatLng(20, 20),
+            zoom: 3,
+            layers: [leafletLayer, heatmapLayer]
+          });
+
+          // Set the data to the data from loadJSON().
+          heatmapLayer.setData(jsonData);
+        });
+      }
+    }
   }
 };
 </script>
