faf1cc1d0d5b4a6cc55e4d7e32fde1a0758fb0c3
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-05 13:45:41 +0100

    Vue + Heatmap.js working

--- vueheatmap/src/components/Heatmap.vue
+++ vueheatmap/src/components/Heatmap.vue
@@ -6,7 +6,8 @@
 
 <script>
 import L from "leaflet";
-import heatmap from "vue-heatmapjs";
+import "heatmap.js";
+import HeatmapOverlay from "leaflet-heatmap";
 
 export default {
   name: "Heatmap",
@@ -21,7 +22,7 @@ export default {
         // Specifies type of data.
         obj.overrideMimeType("application/json");
         // Get the JSON-file.
-        obj.open("GET", "../Dataset/dataset.json", true);
+        obj.open("GET", "./dataset.json", true);
         // EventHandler for readystatechange.
         obj.onreadystatechange = function() {
           // Defining the error.
@@ -32,52 +33,52 @@ export default {
         };
         // Initiates request.
         obj.send(null);
-
-        loadJSON(function(response) {
-          // Parse the response in JSON-format.
-          let jsonResponse = JSON.parse(response);
-
-          // Store the parsed JSON-data.
-          let jsonData = {
-            data: jsonResponse
-          };
-
-          // Create LeafletLayer that holds the actual map.
-          let leafletLayer = L.tileLayer(
-            "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
-            {
-              attribution: "Vue.js + Heatmap.js",
-              maxZoom: 12
-            }
-          );
-
-          // Variable for config-values
-          let config = {
-            radius: 0.25,
-            maxOpacity: 0.8,
-            blur: 0.3,
-            scaleRadius: true,
-            useLocalExtrema: true,
-            // Linking JSON-file-values to Heatmap.js variables.
-            latField: "latitude",
-            lngField: "longitude",
-            valueField: "brightness"
-          };
-
-          // Create Layer and place config-variable in that overlay.
-          let heatmapLayer = new HeatmapOverlay(config);
-
-          // Create Map with Leaflet.js and set view, zoom and layers.
-          let map = new L.Map("map", {
-            center: new L.LatLng(20, 20),
-            zoom: 3,
-            layers: [leafletLayer, heatmapLayer]
-          });
-
-          // Set the data to the data from loadJSON().
-          heatmapLayer.setData(jsonData);
-        });
       }
+
+      loadJSON(function(response) {
+        // Parse the response in JSON-format.
+        let jsonResponse = JSON.parse(response);
+
+        // Store the parsed JSON-data.
+        let jsonData = {
+          data: jsonResponse
+        };
+
+        // Create LeafletLayer that holds the actual map.
+        let leafletLayer = L.tileLayer(
+          "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png",
+          {
+            attribution: "Vue.js + Heatmap.js",
+            maxZoom: 12
+          }
+        );
+
+        // Variable for config-values
+        let config = {
+          radius: 0.25,
+          maxOpacity: 0.8,
+          blur: 0.3,
+          scaleRadius: true,
+          useLocalExtrema: true,
+          // Linking JSON-file-values to Heatmap.js variables.
+          latField: "latitude",
+          lngField: "longitude",
+          valueField: "brightness"
+        };
+
+        // Create Layer and place config-variable in that overlay.
+        let heatmapLayer = new HeatmapOverlay(config);
+
+        // Create Map with Leaflet.js and set view, zoom and layers.
+        new L.Map("map", {
+          center: new L.LatLng(20, 20),
+          zoom: 3,
+          layers: [leafletLayer, heatmapLayer]
+        });
+
+        // Set the data to the data from loadJSON().
+        heatmapLayer.setData(jsonData);
+      });
     }
   }
 };
@@ -87,5 +88,9 @@ export default {
 #map {
   width: 100vw;
   height: 100vh;
+}
+
+.leaflet-container {
+  background: rgba(0, 0, 0, 0.8) !important;
 }
 </style>
