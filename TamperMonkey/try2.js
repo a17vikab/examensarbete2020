efc8831739e767818001ed00ebcc91ae701602db
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-09 14:05:26 +0100

    End of day, remove old tries

--- TamperMonkey/try2.js
+++ TamperMonkey/try2.js
@@ -1,28 +1,48 @@
 // ==UserScript==
-// @name        heatmapMeasurement.js
+// @name        MeasurementTry4.js
 // @namespace   http://tampermonkey.net/
-// @version     0.1
+// @version     1
 // @description Measurement-script for heatmap-data.
 // @author      Viktor Abrahamsson
+// @require     https://code.jquery.com/jquery-3.4.0.js
 // @match       http://127.0.0.1:5500/HeatmapJS/index.html
+// grant        none
 // ==/UserScript==
 
-(function() {
-  "use strict";
-  var counter = localStorage.getItem("counter");
-  if (counter <= 10 || counter == null) {
-    var t0 = localStorage.getItem("t0");
-    var t1 = localStorage.getItem("t1");
-    var result = t1 - t0;
-    localStorage.setItem("result", result);
-    counter++;
-    localStorage.setItem("counter", counter);
-    window.location.reload();
-  } else {
-    var data = "";
-    for (let i = 1; i < counter; i++) {
-      data += localStorage.getItem("result" + i) + "\n";
+let t0;
+let t1;
+let timeArray =
+  localStorage.getItem("timeArray") != null
+    ? JSON.parse(localStorage.getItem("timeAray"))
+    : [];
+
+$("#startMeasurement").click(function() {
+  t0 = new Date().getTime();
+});
+
+$("body").ready(function() {
+  setTimeout(function() {
+    $("#startMeasurement").click();
+  }, 500);
+});
+
+$(document).ajaxComplete(function() {
+  window.setTimeout(function() {
+    if (timeArray.lenght < 10) {
+      t1 = new Date().getTime();
+      let result = t1 - t0;
+      timeArray.push(result);
+      let timeString = JSON.stringify(timeArray);
+      localStorage.setItem("timeArray", timeString);
+      localtion.reload();
+    } else {
+      console.log(timeArray);
+      let text = "";
+      for (let i = 0; i < timeArray.lenght; i++) {
+        text += timeArray[i] + "\n";
+      }
+      console.log(text);
+      localStorage.clear();
     }
-    console.log(data);
-  }
-})();
+  }, 1000);
+});
