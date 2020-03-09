9e244381590bef27d07cabd38330a85c9ef0268f
Viktor Abrahamsson <a17vikab@student.his.se>
2020-03-09 08:53:37 +0100

    Create button & start measurement script

--- TamperMonkey/heatmapMeasurement.js
+++ TamperMonkey/heatmapMeasurement.js
@@ -12,5 +12,52 @@
 
 (function() {
   "use strict";
+  // Variables
+  const URL =
+    "https://wwwlab.iit.his.se/a17vikab/examensarbete/dataReceiver.php";
+  let scrapedData = [];
+  let runs = 10;
+  let counter = localstorage.getItem("counter");
+  const measurementButton = document.getElementById("startMeasurement");
+  let t0;
+  let t1;
 
+  function ajaxCall(data) {
+    try {
+      GM_xmlhttpRequest({
+        method: "POST",
+        url: URL,
+        data: "str=" + encodeURIComponent(data),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+    } catch (ex1) {
+      console.log(ex1);
+    }
+  }
+
+  if (window.addEventListener) {
+    t0 = new Date();
+    localStorage.setItem("Start: ", t0);
+
+    window.addEventListener("load", loadWindow, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onload", loadWindow);
+  }
+
+  measurementButton.addEventListener("click", function() {});
+
+  if (counter == null) {
+    counter = 0;
+    localStorage.setItem("counter", counter);
+  }
+
+  function loadWindow(event) {
+    // Get localStorage for counter.
+    counter = localStorage.getItem("counter");
+
+    if (counter < runs) {
+    }
+  }
 })();
